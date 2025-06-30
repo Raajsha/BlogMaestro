6a067d82bcdb10d6efd2810392b5084ead9aafc0
RaajSha <rsjshahah@gmail.com>
2025-06-30 16:57:43 +0530

    Adding build script

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(cors({
-    origin: "http://localhost:5173" || process.env.CLIENT_URL,
+    origin: "https://blog-maestro-982vbjjkg-raajs-projects-d77e53fa.vercel.app/",
     credentials: true,
 }));
 app.use(express.json());
