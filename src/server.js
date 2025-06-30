4cf276f816b4a3306f1a6e01709e470fc3386706
RaajSha <rsjshahah@gmail.com>
2025-06-30 17:44:33 +0530

    Adding production url

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(cors({
-    origin: "https://blog-maestro-ktv2pk0xz-raajs-projects-d77e53fa.vercel.app/",
+    origin: "https://blog-maestro.vercel.app/",
     credentials: true,
 }));
 app.use(express.json());
