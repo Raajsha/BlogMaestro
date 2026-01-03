8ede0dd7c38b80a769ffe5e7903afa7d51c12a50
RaajSha <rsjshahah@gmail.com>
2026-01-03 11:29:25 +0530

    Updating cors url

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://blog-maestro.vercel.app",
     credentials: true,
 }));
 app.use(express.json());
