70f6aa012f0f287c7b90c60914cf4aec6fe9cacf
Raaj <rsjshahah@gmail.com>
2026-07-02 11:02:12 +0530

    Fixed navbar routing logic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
                             </Link>
 
                             <Link
-                            to = '/register'
+                            to = '/'
                             className='bg-primary-600 text-white px-4 py-2 rounded-lg hover:bg-primary-700 transition-colors'>
                                 Sign Up
                             </Link>
