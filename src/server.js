945e939940820f708a6ca11f2e6d4ac2a6077051
Raaj Shah <rsjshahah@gmail.com>
2026-07-02 11:17:11 +0530

    Add authentication middleware to server

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import authRoutes from './routes/authRoutes.js';
 import commentRoutes from './routes/commentRoutes.js';
 import postRoutes from './routes/postRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import authMiddleware from './middleware/auth.js';
 
 dotenv.config();
 const app = express();
@@ -17,7 +18,7 @@ app.use(cors({
     credentials: true,
 }));
 app.use(express.json());
-
+app.use(authMiddleware);
 app.use('/api/auth', authRoutes);
 app.use('/api/comments',commentRoutes);
 app.use('/api/posts',postRoutes);
